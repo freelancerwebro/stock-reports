45ccbb263bb6e9173d5bd2e0eb625619dcdf44f4
Sorin Marian <sorin.marian@bitpanda.com>
2022-09-28 10:47:14 +0300

    Add unit tests for submit form

--- tests/Feature/SubmitFormTest.php
+++ tests/Feature/SubmitFormTest.php
@@ -9,8 +9,45 @@ class SubmitFormTest extends TestCase
     public function test_success_open_index_page()
     {
         $response = $this->get('/');
+
         $response->assertStatus(200);
         $response->assertSee('Stock historical data');
         $response->assertSee('Submit');
+    }
+
+    public function test_fail_with_empty_params()
+    {
+        $response = $this->json('POST', '/submit', []);
+
+        $response->assertStatus(422);
+        $response->assertJsonStructure([
+           'errors' => [
+               'email',
+               'end_date',
+               'start_date',
+               'symbol',
+           ]
+        ]);
+    }
+
+    public function test_fail_with_invalid_symbol()
+    {
+        $response = $this->json('POST', '/submit', [
+            'symbol' => 'AAAA',
+            'start_date' => '2022-09-01',
+            'end_date' => '2022-09-25',
+            'email' => 'test@test.com',
+        ]);
+
+        $response->assertStatus(422);
+        $response->assertJsonStructure([
+            'errors' => [
+                'symbol',
+            ]
+        ]);
+        $this->assertEquals(
+            'The selected symbol is invalid.',
+            $response->getData()->errors->symbol[0],
+        );
     }
 }
