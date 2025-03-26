5f2508dec08add6289ed779e3f8c2c371349f92f
Sorin Marian <marian.sorin@yahoo.com>
2025-03-26 16:31:29 +0200

    Include seeders in the setup

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,9 @@
+echo 'Prepare config files...'
 cp .env.example .env
 
+echo 'Generate key...'
 php artisan key:generate
 
+echo 'Building containers...'
 docker compose build --no-cache
 docker compose up -d
-
