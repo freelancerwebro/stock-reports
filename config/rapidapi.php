275359d5c83fbd0f4521e3fa8de418b815284631
Sorin Marian <marian.sorin@yahoo.com>
2025-03-28 16:04:40 +0200

    Update rapidApi format to a new one

--- config/rapidapi.php
+++ config/rapidapi.php
@@ -3,7 +3,7 @@
 declare(strict_types=1);
 
 return [
-    'base_uri' => env('RAPID_API_BASE_URI', 'https://yh-finance.p.rapidapi.com/stock/v3/get-historical-data'),
-    'header_host' => env('RAPID_API_HEADER_HOST', 'yh-finance.p.rapidapi.com'),
+    'base_uri' => env('RAPID_API_BASE_URI', 'https://yahoo-finance15.p.rapidapi.com/api/v1/markets/stock/history'),
+    'header_host' => env('RAPID_API_HEADER_HOST', 'yahoo-finance15.p.rapidapi.com'),
     'header_key' => env('RAPID_API_HEADER_KEY'),
 ];
